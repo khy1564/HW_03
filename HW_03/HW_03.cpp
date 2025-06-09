25dbee51e4ab7f913ecf1f0c39049c8d9eb66910
khy1564 <comkhy@gmail.com>
2025-06-09 17:26:53 +0900

    HW_03

--- HW_03/HW_03.cpp
+++ HW_03/HW_03.cpp
@@ -1,9 +1,38 @@
-﻿#include <iostream>
+﻿#include "HW_03.h"
 
-using namespace std;
 
 int main()
 {
-    
+	SimpleVector<int> sim(3);
+
+	sim.push_back(5);
+	sim.push_back(12);
+	sim.push_back(2);
+	sim.push_back(30);
+	sim.push_back(20);
+	sim.push_back(33);
+
+	cout << "생성자" << endl;
+	cout << "Szie : " << sim.size() << endl;
+	cout << "Capacity : " << sim.capacity() << endl;
+	
+	sim.pop_back();
+	cout << "Szie : " << sim.size() << endl;
+
+	SimpleVector<int> sim2 = sim;
+
+	sim2.push_back(334);
+
+	cout << "\n복사생성자" << endl;
+	cout << "Szie : " << sim2.size() << endl;
+	cout << "Capacity : " << sim2.capacity() << endl;
+
+	cout << "\n정렬" << endl;
+	cout << "sim : ";
+	sim.sortData();
+	cout << "\nsim2 : ";
+	sim2.sortData();
+	
+
 }
 
---
+++ HW_03/HW_03.h
@@ -0,0 +1,108 @@
+#pragma once
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+template <typename T>
+
+class SimpleVector
+{
+public:
+
+	SimpleVector() : currentSize(0), currentCapacity(10)
+	{
+		data = new T[currentCapacity];
+	};
+
+	SimpleVector(int capacity) : currentSize(0), currentCapacity(capacity)
+	{
+		data = new T[currentCapacity];
+	}
+
+	SimpleVector(const SimpleVector& other) : currentSize(other.currentSize), currentCapacity(other.currentCapacity)
+	{
+		data = new T[currentCapacity];
+		for (int i = 0; i < currentSize; i++)
+		{
+			data[i] = other.data[i];
+		}
+	}
+
+	~SimpleVector()
+	{
+		delete[] data;
+	};
+
+	void push_back(const T& value)
+	{
+		if (currentSize >= currentCapacity)
+		{
+			resize(currentCapacity + 5);
+		}
+		data[currentSize++] = value;
+
+	}
+
+	void pop_back()
+	{
+		if (currentSize > 0)
+		{
+			--currentSize;
+		}
+
+	}
+
+	int size()
+	{
+		return currentSize;
+	}
+
+	int capacity()
+	{
+		return currentCapacity;
+	}
+
+	void sortData() 
+	{
+		sort(data, data + currentSize);
+		for (int i = 0; i < currentSize; i++)
+		{
+			cout << data[i] << " ";
+		}
+		cout << endl;
+	}
+
+	void print()
+	{
+		for (int i = 0; i < currentSize; i++)
+		{
+			cout << data[i] << endl;
+		}
+	}
+
+private:
+	T* data;
+	int currentSize;
+	int currentCapacity;
+
+	void resize(int newCapacity) 
+	{
+		if (newCapacity <= currentCapacity)
+		{
+			return;
+		}
+
+		T* newData = new T[newCapacity];
+		for (int i = 0; i < currentSize; i++)
+		{
+			newData[i] = data[i];
+		}
+
+		delete[] data;
+		data = newData;
+		currentCapacity = newCapacity;
+	}
+};
+
